48790f219176070f88d834008b64cc03408a75ec
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-01-28 00:02:56 +0530

    Add files via upload

---
+++ delivery.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int orderAmount, speedType;
+
+    scanf("%d", &orderAmount);
+    scanf("%d", &speedType);
+
+    switch (speedType)
+    {
+        case 1:
+            printf("Delivery ₹50");
+            break;
+
+        case 2:
+            if (orderAmount < 1000)
+                printf("Delivery ₹100");
+            else
+                printf("Delivery Free");
+            break;
+
+        default:
+            printf("Invalid speed type");
+    }
+
+    return 0;
+}
