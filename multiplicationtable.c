00c7b5c67bfb54ed65fcdd78401868ff388dd0c2
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-01-29 05:01:01 +0530

    Add files via upload

---
+++ multiplicationtable.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int product = 1;
+    scanf("%d", &n);
+    for (int i=1; i <= 10; i++) {
+        product = n*i;
+        printf("%d ", product);
+    }
+
+    return 0;
+}
