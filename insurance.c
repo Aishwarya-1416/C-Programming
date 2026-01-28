1b260b867057da0c84520da8929f529cdc28439c
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-01-28 06:08:50 +0530

    Add files via upload

---
+++ insurance.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int category, age;
+
+    scanf("%d", &category);
+    scanf("%d", &age);
+
+    switch (category) {
+        case 1:   // Two Wheeler
+            if (age <= 5)
+                printf("₹1500");
+            else
+                printf("₹2500");
+            break;
+
+        case 2:   // Four Wheeler
+            if (age <= 5)
+                printf("₹4000");
+            else
+                printf("₹6000");
+            break;
+    }
+
+    return 0;
+}
