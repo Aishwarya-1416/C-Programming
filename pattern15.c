98249a072f19710b5c9f45851c81ccc27de17b67
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-01-29 22:04:41 +0530

    Add files via upload

---
+++ pattern15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int n = 4;
+    for (int i = 0; i < n; i++) {
+        char ch = 'A';
+        for (int j = 0; j <= i; j++)
+            printf("%c", ch++);
+        ch -= 2;
+        for (int j = 0; j < i; j++)
+            printf("%c", ch--);
+        printf("\n");
+    }
+    return 0;
+}
