1da0333d72b423229c1e5c876760c5bc22b05403
Aishwarya-1416 <aishusrini1416@gmail.com>
2026-02-16 13:54:29 +0530

    Add files via upload

---
+++ pointers1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void swap(int *x, int *y) {
+    int temp=*x;
+    *x = *y;
+    *y=temp;
+}
+int main() {
+    int a,b;
+    scanf("%d %d", &a, &b);
+    printf("Before Swapping %d %d\n", a,b);
+    swap(&a, &b);
+    printf("After Swapping %d %d", a,b);
+    return 0;
+}
+
